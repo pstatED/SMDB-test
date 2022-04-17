de3053060560d9fc63daa4d2f15971d832d03a69
EURODYN\pstat <panagiotis.stathopoulos@eurodyn.com>
2022-04-17 22:45:22 +0300

    Initial commit

---
+++ src/main/java/com/example/smdbtest/SmdbTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.smdbtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmdbTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmdbTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/smdbtest/SmdbTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smdbtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmdbTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
